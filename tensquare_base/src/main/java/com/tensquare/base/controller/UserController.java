c76202eff89dcc01151a93803c18e78711f2b821
stan <1181092507@qq.com>
2019-08-13 11:11:06 +0800

    test interface success for CRUD BASE

--- tensquare_base/src/main/java/com/tensquare/base/controller/UserController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.tensquare.base.controller;
 
+import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /*
 标签控制层
@@ -34,4 +32,31 @@ public class UserController {
         return new Result(true,StatusCode.OK,"查询成功",labelService.findById(id));
     }
 
+    /*
+    增加标签
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public Result add(@PathVariable Label label){
+        labelService.add(label);
+        return new Result(true,StatusCode.OK,"增加成功");
+    }
+
+    /*
+    修改标签
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public Result update(@RequestBody Label label, @PathVariable String id){
+        label.setId(id);
+        labelService.update(label);
+        return new Result(true,StatusCode.OK,"修改成功");
+    }
+
+    /*
+    删除label
+     */
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public Result deleteById(@PathVariable String id){
+        labelService.deleteById(id);
+        return new Result(true,StatusCode.OK,"删除成功");
+    }
 }
