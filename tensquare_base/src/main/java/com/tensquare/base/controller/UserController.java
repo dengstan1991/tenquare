4bf6ab9f59b5d8ef19b7d331b8c0379b4e027572
stan <1181092507@qq.com>
2019-08-13 15:01:58 +0800

    add 查询接口

--- tensquare_base/src/main/java/com/tensquare/base/controller/UserController.java
+++ tensquare_base/src/main/java/com/tensquare/base/controller/UserController.java
@@ -2,10 +2,14 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /*
 标签控制层
@@ -58,5 +62,23 @@ public class UserController {
     public Result deleteById(@PathVariable String id){
         labelService.deleteById(id);
         return new Result(true,StatusCode.OK,"删除成功");
+    }
+
+    /**
+     * 根据条件查询
+     */
+    @RequestMapping(value = "/search",method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Map searchMap){
+        return new Result(true,StatusCode.OK,"查询成功",labelService.findSearch(searchMap));
+
+    }
+    /**
+     * 根据条件查询+分页
+     */
+    @RequestMapping(value = "/search/{page}/{size}",method = RequestMethod.POST)
+    public Result findSearch(@RequestBody Map searchMap,@PathVariable int page,@PathVariable int size){
+        Page page1ist=labelService.findSearch(searchMap,page,size);
+        return new Result(true,StatusCode.OK,"查询成功"
+        ,new PageResult<>(page1ist.getTotalElements(), page1ist.getContent()));
     }
 }
--- tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,10 +3,19 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 业务逻辑
@@ -19,34 +28,78 @@ public class LabelService {
     private IdWorker idWorker;
 
     /**
-     * 查询全部标签
+     * 查询全部标签label
      */
     public List<Label> findAll(){
         return labelDao.findAll();
     }
     /*
-    根据id查询
+    根据id查询label
      */
     public Label findById(String id){
         return labelDao.findById(id).get();
     }
     /**
-     * 增加
+     * 增加label
      */
     public void add(Label label){
         label.setId(idWorker.nextId()+"");
         labelDao.save(label);
     }
     /**
-     * 修改
+     * 修改label
      */
     public void update(Label label){
         labelDao.save(label);
     }
     /**
-     * 删除
+     * 删除label
      */
     public void deleteById(String id){
         labelDao.deleteById(id);
     }
+
+    /*
+    构建查询条件 jpa的复杂查询
+     */
+    private Specification<Label> createSpecification(Map searchMap){
+        return new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder cb) {
+                List<Predicate> predicateList=new ArrayList<>();
+                if(searchMap.get("labelname")!=null &&!"".equals(searchMap.get("label"))){
+                    predicateList.add(cb.like(root.get("labelname").as(String.class),
+                            "%"+(String)searchMap.get("labelname")+"%"
+                    ));
+                }
+                if(searchMap.get("state")!=null &&!"".equals(searchMap.get("state"))){
+                    predicateList.add(cb.like(root.get("state").as(String.class),
+                            "%"+(String)searchMap.get("state")+"%"
+                    ));
+                }
+                if(searchMap.get("recommend")!=null &&!"".equals(searchMap.get("recommend"))){
+                    predicateList.add(cb.like(root.get("recommend").as(String.class),
+                            "%"+(String)searchMap.get("recommend")+"%"
+                    ));
+                }
+                return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
+            }
+        };
+    }
+    /*
+    条件查询
+     */
+    public List<Label> findSearch(Map searchMap){
+    Specification specification=createSpecification(searchMap);
+    return labelDao.findAll(specification);
+     }
+
+     /*
+     分页查询org.springframework.data
+      */
+     public Page<Label> findSearch(Map searchMap,int page,int size){
+         Specification specification=createSpecification(searchMap);
+         PageRequest pageRequest=PageRequest.of(page-1,size);
+         return labelDao.findAll(specification,pageRequest);
+     }
 }
