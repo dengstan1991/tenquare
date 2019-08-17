3ad593690c3db667f85e72e35f2fba729bd24c67
Administrator <1181092507@qq.com>
2019-08-17 21:16:07 +0800

    add 注册 发短信功能开发

---
+++ tensquare_sms/src/main/java/com/tensquare/sms/smsApplication.java
@@ -0,0 +1,13 @@
+package com.tensquare.sms;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class smsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(smsApplication.class,args);
+    }
+
+}
